2695947d2a84715224b844676139f9779f63f6b1
hyskc <157987756+hyskc@users.noreply.github.com>
2026-04-09 13:09:48 +0800

    Delete xiaosuteacher directory

--- xiaosuteacher/xiaosu/app/src/main/java/com/example/xiaosu/model/Resource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.xiaosu.model
-
-data class Resource(
-    val id: String,
-    val title: String,
-    val description: String,
-    val date: String,
-    val downloadUrl: String
-)
